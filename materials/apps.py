4259b193e3d5a7be10a25e58cc34bc0cb8904ae6
abdulhafizfambali <abdulhafizfabali@gmail.com>
2026-04-18 13:18:08 +0000

    Fix materials/ apps.py

--- materials/apps.py
+++ materials/apps.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class MaterialsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'materials'
-=======
-from django.apps import AppConfig
-
-
-class MaterialsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'materials'
->>>>>>> a5fbf081a135469b7d52e7f7e05a2c01f65b12b5
